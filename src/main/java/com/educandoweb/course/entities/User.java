2fe725698e9949078a2da601ad78792cfde8ebd0
Gabriel Silva <gabrielfelipecarvalho1@gmail.com>
2023-09-19 22:55:13 -0300

    Criação de entidade Order, seeding e definicao de formato de hora - iso 8601

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,12 +1,17 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -23,6 +28,10 @@ public class User implements Serializable {
 	private String fone;
 	private String password;
 
+	@JsonIgnore
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+	
 	public User() {
 	}
 
@@ -69,6 +78,10 @@ public class User implements Serializable {
 
 	public String getPassword() {
 		return password;
+	}
+	
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	public void setPassword(String password) {
